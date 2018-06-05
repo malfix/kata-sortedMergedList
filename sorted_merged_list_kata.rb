82c78f5f58cadf89881f7abbad67d02b66e2d551
Emanuele Malfara <emanuele.malfara@trovaprezzi.it>
2018-06-05 08:39:31 +0200

    test verdi, da rifattorizzare

--- sorted_merged_list_kata.rb
+++ sorted_merged_list_kata.rb
@@ -3,7 +3,34 @@ class SortedMergedListKata
   def initialize
   end
 
-  def sortit(arrays)
-    []
+  def sortit(*arrays)
+    return [] if (arrays.nil? || arrays.size == 0)
+    next_index = [0] * arrays[0].size
+    sorted_list = []
+    while(true) do
+      idx_lower = pick_lower_element_idx(arrays, next_index)
+      if idx_lower
+        sorted_list << arrays[0][idx_lower][next_index[idx_lower]]
+        next_index[idx_lower] += 1
+      else
+        return sorted_list
+      end
+    end
+    return sorted_list
   end
+
+  private
+  def pick_lower_element_idx(arrays, next_index)
+    next_lower = nil
+    arrays[0].each_with_index do |original_list, idx|
+      if next_index[idx] && original_list[next_index[idx]]
+        next_lower = idx if next_lower.nil?
+        if arrays[0][next_lower][next_index[next_lower]] > original_list[next_index[idx]]
+          next_lower = idx
+        end
+      end
+    end
+    next_lower
+  end
+
 end
--- test_sorted_merged_list_kata.rb
+++ test_sorted_merged_list_kata.rb
@@ -2,7 +2,25 @@ require_relative "sorted_merged_list_kata"
 require "test/unit"
 
 class TestSortedMergedListKata < Test::Unit::TestCase
-  def test_base
-    assert_equal([], SortedMergedListKata.new().sortit([]) )
+  def test_empty
+    assert_equal([], SortedMergedListKata.new().sortit() )
+    assert_equal([], SortedMergedListKata.new().sortit([]))
+    assert_equal([], SortedMergedListKata.new().sortit([[]]))
+    assert_equal([], SortedMergedListKata.new().sortit([[],[]]) )
   end
+
+  def test_one_element
+    assert_equal([1], SortedMergedListKata.new().sortit([[],[1]]) )
+    assert_equal([1], SortedMergedListKata.new().sortit([[1]]) )
+  end
+
+  def test_different_size
+    assert_equal([1, 1, 1, 3, 5, 10, 20, 30, 40], SortedMergedListKata.new().sortit([[1], [1, 3, 5], [1, 10, 20, 30, 40]]) )
+  end
+
+  def test_same_size
+    assert_equal([10, 12, 15, 15, 17, 20, 20, 30, 32], SortedMergedListKata.new().sortit([[10, 15, 30], [12, 15, 20], [17, 20, 32]]) )
+  end
+
+
 end
